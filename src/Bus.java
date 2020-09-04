848f1a88d244b0eae9933a8eaf29356334cf8cb1
yacooler <yacooler@yandex.ru>
2020-09-04 22:22:36 +0300

    Готово

---
+++ src/Bus.java
@@ -0,0 +1,6 @@
+public class Bus extends Machine{
+    public Bus(String uniqueName, Road road) {
+        super(40F, 40F, 7.5F, uniqueName, road);
+        machineName = "Автобус";
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,6 @@
+public class Truck extends Machine{
+    public Truck(String uniqueName, Road road) {
+        super(60F, 60F, 15F, uniqueName, road);
+        machineName = "Грузовой автомобиль";
+    }
+}
